e83bbb6d173545e90c7dbd35ebc1fa2660a41d1f
MagdaLiskiewicz <156846346+MagdaLiskiewicz@users.noreply.github.com>
2024-10-09 19:14:36 +0200

    5

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -2,7 +2,7 @@ import ErrorMessage from "../../components/ErrorMessage/ErrorMessage.jsx";
 import Section from "../../components/Section/Section.jsx";
 import Container from "../../components/Container/Container.jsx";
 import css from "./NotFoundPage.module.css";
-import image from "../../assets/night.webp";
+// import image from "../../assets/night.webp";
 import { NavLink } from "react-router-dom";
 
 const NotFoundPage = () => {
@@ -10,13 +10,13 @@ const NotFoundPage = () => {
     <Section>
       <Container>
         <div className={css.imageBox}>
-          <div className={css.imageWrapper}>
+          {/* <div className={css.imageWrapper}>
             <img
               src={image}
               alt="the night in the wood"
               className={css.image}
             />
-          </div>
+          </div> */}
           <div className={css.titleWrapper}>
             <h1 className={css.title}>404 ERROR</h1>
             <NavLink className={css.btn} to="/">
